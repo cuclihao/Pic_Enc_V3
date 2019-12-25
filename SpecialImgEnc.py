97a16a9b27e80da3d638d0c5bf24cba3a53b871e
cuclihao <33593359+cuclihao@users.noreply.github.com>
2019-12-25 10:55:22 +0800

    Add files via upload

---
+++ SpecialImgEnc.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 12 10:25:59 2019
+
+#CASE: when the with and hight of image are not the integral multiple of 4  
+
+
+@author: cucli
+"""
+import sys
+from sys import path
+from .PicEnc_V3_Finally import *
+
+SpeedTest()
+
